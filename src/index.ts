4b6227cf9a9034f2e1e59592260e81648036d9f7
norbert.mosinski <mosinski@lean-ocean.com>
2023-07-29 07:29:29 +0200

    main: Removes index.ts

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { AxiosAdapter } from './adapters/axios-adapter';
-import { RouteBuildService } from './services/route-build-service';
-
-export const client = new RouteBuildService();
-
-export const axiosAdapter = new AxiosAdapter();
