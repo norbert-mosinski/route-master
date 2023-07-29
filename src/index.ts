7f786405860a223a312cb398af2d900532cafd49
norbert.mosinski <mosinski@lean-ocean.com>
2023-07-29 07:55:13 +0200

    main: Adds index.ts in order to allow better usability.

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { HttpMethod } from './enums/http-method';
+import { AxiosAdapter } from './adapters/axios-adapter';
+import { FetchAdapter } from './adapters/fetch-adapter';
+import { RouteBuildService } from './services/route-build-service';
+
+export {HttpMethod, AxiosAdapter, FetchAdapter, RouteBuildService};
